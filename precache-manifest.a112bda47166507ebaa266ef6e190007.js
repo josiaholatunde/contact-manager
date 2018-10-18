28014967a3620557f0632cb8bb52634ab2d58b1b
Ogunboyejo Olatunde <Ogunboyejo@tdafrica.com>
2018-10-18 10:03:50 +0100

    Updates

---
+++ precache-manifest.a112bda47166507ebaa266ef6e190007.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contact-manager/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6289640685a4ea806583",
+    "url": "/contact-manager/static/css/main.9978e5ca.chunk.css"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contact-manager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "cce32112d0d6450254c1",
+    "url": "/contact-manager/static/js/1.cce32112.chunk.js"
+  },
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "6289640685a4ea806583",
+    "url": "/contact-manager/static/js/main.62896406.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contact-manager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contact-manager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contact-manager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "cce32112d0d6450254c1",
+    "url": "/contact-manager/static/css/1.f571ab70.chunk.css"
+  },
+  {
+    "revision": "a9aa8ee00aba718332a33435599f0ed1",
+    "url": "/contact-manager/index.html"
+  }
+];
