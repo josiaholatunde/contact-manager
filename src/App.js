526827c5f890d1dcdac342a47eae5c835baea224
Ogunboyejo Olatunde <Ogunboyejo@tdafrica.com>
2018-10-18 10:21:06 +0100

    slight change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Contacts from './containers/Contacts';
 import Header from './components/Header';
 import { Provider } from './Context';
